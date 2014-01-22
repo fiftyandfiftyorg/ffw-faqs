97dcabb9316732ba24bbf85c6dc62dab0607da83
bryanmonzon <monzonb@gmail.com>
2014-01-21 16:40:05 -0800

    refactored shortcodes to include oderby and order options for both taxonomony level and faq post type level

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -6,13 +6,83 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-function ffw_faqs_list_shortcode( $post ){
+function ffw_faqs_list_shortcode( $atts, $content=null){
 
     global $post;
 
-    ob_start(); 
+    extract( shortcode_atts( array(
+        'topic_orderby' => 'title',
+        'topic_order'   => 'ASC',
+        'orderby'       => 'date',
+        'order'         => 'ASC',
+        'topic'         => ''
+    // ...etc
+    ), $atts ) );
+
+     
     
-    ffw_faqs_get_template_part( 'faq', 'list' );
+    $post_type = 'ffw_faqs';
+
+    // Get all the taxonomies for this post type
+    $taxonomies = get_object_taxonomies( array( 'post_type' => $post_type ) );
+
+    foreach( $taxonomies as $taxonomy ) { 
+
+        
+        // Gets every "category" (term) in this taxonomy to get the respective posts
+        $term_args = array(
+                'orderby'   => $topic_orderby,
+                'order'     => $topic_order
+                );
+
+        $terms = get_terms( $taxonomy, $term_args );
+        
+
+        foreach( $terms as $term ) { 
+
+            $args = array(
+                'taxonomy'  => $taxonomy,
+                'term'      => $term->slug,
+                'orderby'   => $orderby,
+                'order'     => $order
+                );
+            $faqs = new WP_Query( $args );
+            
+            if( $faqs->have_posts() ) : ?>
+                <h3><?php echo $term->name; ?></h3>
+                
+                <?php ob_start(); ?>
+                
+                <div class="ffw-faqs-accordion">
+
+                <?php while( $faqs->have_posts() ) : $faqs->the_post(); ?>
+            
+                        
+                    <h3 class="ffw-faqs-accordion-trigger"><a href="#"><?php the_title(); ?></a></h3>
+                    <div>
+                    <?php 
+
+                    if( ffw_faqs_is_excerpt_set() ) {
+                        the_excerpt();
+                    ?>
+                    <div class="ffw_faqs_wrap">
+                        <a href="<?php the_permalink(); ?>">Read More</a>
+                    </div>
+                    <?php
+                    } else{
+                        the_content(); 
+                     }
+                    ?>
+                    </div>
+
+            
+                <?php endwhile; ?>
+                </div>
+            <?php endif;
+
+        }
+
+    }
 
     $faq_list = ob_get_clean();
 
@@ -20,20 +90,3 @@ function ffw_faqs_list_shortcode( $post ){
 }
 add_shortcode('faq_list', 'ffw_faqs_list_shortcode');
 
-
-
-function ffw_faqs_search_shortcode( $post ) 
-{
-    global $post, $ffw_faqs_settings;
-
-    ob_start();
-
-    ffw_faqs_get_template_part( 'faq', 'search' );
-
-    $faq_search = ob_get_clean();
-
-    return $faq_search;
-
-
-}
-add_shortcode('faq_search', 'ffw_faqs_search_shortcode');

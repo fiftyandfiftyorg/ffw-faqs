1c289dfb9f00deb515c649efaf967e2603baa3ac
bryanmonzon <monzonb@gmail.com>
2014-01-29 16:16:51 -0800

    function comments

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -19,68 +19,58 @@ function ffw_faqs_list_shortcode( $atts, $content=null){
     // ...etc
     ), $atts ) );
 
-     
+        
+    // Gets every "category" (term) in this taxonomy to get the respective posts
+    $term_args = array(
+            'orderby'   => $topic_orderby,
+            'order'     => $topic_order
+            );
+
+
+    $terms = get_terms( 'faq_topics', $term_args );
     
-    $post_type = 'ffw_faqs';
 
-    // Get all the taxonomies for this post type
-    $taxonomies = get_object_taxonomies( array( 'post_type' => $post_type ) );
+    foreach( $terms as $term ) { 
 
-    foreach( $taxonomies as $taxonomy ) { 
-
+        $args = array(
+            'taxonomy'  => 'faq_topics',
+            'term'      => $term->slug,
+            'orderby'   => $orderby,
+            'order'     => $order
+            );
+        $faqs = new WP_Query( $args );
         
-        // Gets every "category" (term) in this taxonomy to get the respective posts
-        $term_args = array(
-                'orderby'   => $topic_orderby,
-                'order'     => $topic_order
-                );
+        if( $faqs->have_posts() ) : ?>
+            <h3><?php echo $term->name; ?></h3>
+            
+            <?php ob_start(); ?>
+            
+            <div class="ffw-faqs-accordion">
 
-        $terms = get_terms( $taxonomy, $term_args );
+            <?php while( $faqs->have_posts() ) : $faqs->the_post(); ?>
         
+                    
+                <h3 class="ffw-faqs-accordion-trigger"><a href="#"><?php the_title(); ?></a></h3>
+                <div>
+                <?php 
 
-        foreach( $terms as $term ) { 
-
-            $args = array(
-                'taxonomy'  => $taxonomy,
-                'term'      => $term->slug,
-                'orderby'   => $orderby,
-                'order'     => $order
-                );
-            $faqs = new WP_Query( $args );
-            
-            if( $faqs->have_posts() ) : ?>
-                <h3><?php echo $term->name; ?></h3>
-                
-                <?php ob_start(); ?>
-                
-                <div class="ffw-faqs-accordion">
-
-                <?php while( $faqs->have_posts() ) : $faqs->the_post(); ?>
-            
-                        
-                    <h3 class="ffw-faqs-accordion-trigger"><a href="#"><?php the_title(); ?></a></h3>
-                    <div>
-                    <?php 
-
-                    if( ffw_faqs_is_excerpt_set() ) {
-                        the_excerpt();
-                    ?>
-                    <div class="ffw_faqs_wrap">
-                        <a href="<?php the_permalink(); ?>">Read More</a>
-                    </div>
-                    <?php
-                    } else{
-                        the_content(); 
-                     }
-                    ?>
-                    </div>
-
-            
-                <?php endwhile; ?>
+                if( ffw_faqs_is_excerpt_set() ) {
+                    the_excerpt();
+                ?>
+                <div class="ffw_faqs_wrap">
+                    <a href="<?php the_permalink(); ?>">Read More</a>
                 </div>
-            <?php endif;
+                <?php
+                } else{
+                    the_content(); 
+                 }
+                ?>
+                </div>
 
-        }
+        
+            <?php endwhile; ?>
+            </div>
+        <?php endif;
 
     }
 
